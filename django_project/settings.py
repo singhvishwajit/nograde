e075398146336aa6c5427a715a50606a3e293b76
Vishwajit Singh <vishwajit7@gmail.com>
2019-12-14 18:27:08 +0530

    Updated settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vhrf3l6gouhmi(9lgefx+#*@mi5n=(eg4d0c^d$0ab@%yf$#2%'
+# SECRET_KEY = 'vhrf3l6gouhmi(9lgefx+#*@mi5n=(eg4d0c^d$0ab@%yf$#2%'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nograde.herokuapp.com']
 
 
 TINYMCE_DEFAULT_CONFIG = {
