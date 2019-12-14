42cf7a93adebfda250e690cd356dd723f0b87aed
Vishwajit Singh <vishwajit7@gmail.com>
2019-12-14 18:04:12 +0530

    Updated STATIC_ROOT

--- django_project/settings.py
+++ django_project/settings.py
@@ -154,7 +154,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
