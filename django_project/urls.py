4c089b36c6dc44388b1310b13d321dbc60cb3195
Vishwajit Singh <vishwajit7@gmail.com>
2020-04-03 20:25:20 +0530

    Cleaned urls

--- django_project/urls.py
+++ django_project/urls.py
@@ -24,7 +24,7 @@ from django.conf import settings
 urlpatterns = [
     path('justice/', admin.site.urls),
     path('', include('blog.urls')),
-    path('learn/', include('learn.urls')),
+    path('essays/', include('learn.urls')),
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html', extra_context = {
--- learn/urls.py
+++ learn/urls.py
@@ -4,12 +4,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('courses/', views.courses, name='learn-courses'),
-	path('courses/entrepreneurship', views.courses_entrepreneurship, name='learn-courses-entrepreneurship'),
-	path('courses/personal-growth', views.courses_personal_growth, name='learn-courses-personal-growth'),
-	path('courses/philosophy', views.courses_philosophy, name='learn-courses-philosophy'),
-	path('courses/psychology', views.courses_psychology, name='learn-courses-psychology'),
-	path('courses/society', views.courses_society, name='learn-courses-society'),
-	path('courses/<slug:slug>/', views.course_details, name='learn-course-details'),
+	path('all', views.courses, name='all-essays'),
+	path('work', views.courses_entrepreneurship, name='essays-work'),
+	path('personal-growth', views.courses_personal_growth, name='essays-personal-growth'),
+	path('thinking', views.courses_philosophy, name='essays-thinking'),
+	path('self-knowledge', views.courses_psychology, name='essays-self-knowledge'),
+	path('society', views.courses_society, name='essays-society'),
+	path('<slug:slug>/', views.course_details, name='essay-details'),
 ]
 
